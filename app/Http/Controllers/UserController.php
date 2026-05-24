a09b0a6e80d498d005ca7e3d84366988a582e016
Bryan Carlie Lukito Setiawan <blukitosetiawan@gmail.com>
2026-05-24 10:03:34 +0700

    fix: import Storage facade in UserController to resolve Class not found error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rules;
 use Illuminate\Support\Str;
 use Maatwebsite\Excel\Facades\Excel;
